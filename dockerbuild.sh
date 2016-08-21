00bd79f1c4eb1c2f562fdeeff5350a07c43c4ba4
jkchr1s <bethel.chris@gmail.com>
2016-08-21 14:38:43 -0400

    Update docker config

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -9,17 +9,17 @@ fi
 # maria db
 MARIACHECK=`docker images | grep mariadb`
 if [ -z "$MARIACHECK" ]; then
-  docker run --net=localdev --name mariadb -e MYSQL_ROOT_PASSWORD=epb123 -e MYSQL_DATABASE=wholesale_mediaroom -e MYSQL_USER=wholesale -e MYSQL_PASSWORD=epb123 -d mariadb:latest
+  docker run --net=localdev --name mariadb -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=site -e MYSQL_USER=site -e MYSQL_PASSWORD=test -d mariadb:latest
 fi
 
 # build the project image
-docker build -t wholesale-mediaroom:latest .
+docker build -t whitepeels:latest .
 
 # create the container
-docker create --name=wholesale-mediaroom --net=localdev -p 8000:8000 -v $(pwd):/home/app wholesale-mediaroom:latest
-docker run --name=wholesale-mediaroom-temp -v $(pwd):/home/app wholesale-mediaroom /usr/local/bin/composer install
-docker rm wholesale-mediaroom-temp
+docker create --name=whitepeels --net=localdev -p 8000:8000 -v $(pwd):/home/app whitepeels:latest
+docker run --name=whitepeels-temp -v $(pwd):/home/app whitepeels /usr/local/bin/composer install
+docker rm whitepeels-temp
 rm -rf .composer
-docker start wholesale-mediaroom
-docker exec wholesale-mediaroom /usr/local/bin/php /home/app/artisan key:generate
-docker exec wholesale-mediaroom /usr/local/bin/php /home/app/artisan migrate
+docker start whitepeels
+docker exec whitepeels /usr/local/bin/php /home/app/artisan key:generate
+docker exec whitepeels /usr/local/bin/php /home/app/artisan migrate
