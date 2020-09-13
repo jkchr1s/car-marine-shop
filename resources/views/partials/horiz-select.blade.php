57cdb4f82f3497f03e4203b296a1d9d21a871907
Chris <bethel.chris@gmail.com>
2020-09-13 17:16:58 -0400

    Re-usable partials for forms/modals

--- resources/views/partials/horiz-select.blade.php
+++ resources/views/partials/horiz-select.blade.php
@@ -3,7 +3,7 @@
     <div class="col-md-10">
     <select id="{{ $name }}" class="form-control" name="{{ $name }}"{!! empty($onChange) ? '' : ' onchange=' . json_encode($onChange)  !!}>
         @foreach($options as $item)
-        <option value="{{ $item->{$optionValue} }}"{{ isset($value) && !empty($value) && strval($value) === strval($item->{$optionValue}) ? ' selected' : '' }}>{{ $item->{$option} }}</option>
+        <option value="{{ $item->{$optionValue} }}"{{ isset($value) && !empty($value) && strval($value) === strval($item->{$optionValue}) ? ' selected="selected"' : '' }}>{{ $item->{$option} }}</option>
         @endforeach
     </select>
     </div>
