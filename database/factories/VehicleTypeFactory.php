756f8ba2d1291d3536b0bd1bbaa785b3a39be1be
Chris <bethel.chris@gmail.com>
2020-09-17 15:17:06 -0400

    Add Vehicle factories

---
+++ database/factories/VehicleTypeFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\VehicleType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class VehicleTypeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = VehicleType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
