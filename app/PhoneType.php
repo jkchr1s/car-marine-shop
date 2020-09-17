64a6ea3ba3170016249464dd47a018d9cd8f63d0
Chris <bethel.chris@gmail.com>
2020-09-17 15:11:02 -0400

    Add HasFactory and fillable

--- app/PhoneType.php
+++ app/PhoneType.php
@@ -2,9 +2,19 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PhoneType extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'type',
+    ];
 }
